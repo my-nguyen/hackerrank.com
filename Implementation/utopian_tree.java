bdfa4be4f30645ad31bb2185483333b9e5eef0ef
iceguyvn <nguyen_my@yahoo.com>
2016-05-29 10:30:31 -0700

    Utopian Tree

--- Implementation/utopian_tree.java
+++ Implementation/utopian_tree.java
@@ -4,55 +4,49 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 
-class MyList
-{
-  private List<Integer> data;
-
-  public MyList()
-  {
-    data = new ArrayList<Integer>();
-  }
-
-  public void read()
-  {
-    Scanner scanner = new Scanner(System.in);
-    // number of integers to fetch next
-    int length = scanner.nextInt();
-    for (int i = 0; i < length; i++)
-      // read all the integers
-      data.add(scanner.nextInt());
-
-    scanner.close();
-  }
-
-  public List<Integer> process()
-  {
-    List<Integer> output = new ArrayList<Integer>();
-    for (Integer cycle : data)
-    {
-      int growth = 1;
-      for (int i = 0; i < cycle; i++)
-      {
-        // spring growth: double in height
-        if (i % 2 == 0)
-          growth *= 2;
-        // summer growth: just add 1 meter
-        else
-          growth += 1;
-      }
-      output.add(growth);
-    }
-    return output;
-  }
-}
-
 class utopian_tree
 {
-  public static void main(String[] args)
-  {
-    MyList list = new MyList();
-    list.read();
-    for (Integer number : list.process())
-      System.out.println(number);
-  }
+   public static void main(String[] args) {
+      process(read());
+   }
+
+   private static List<Case> read() {
+      List<Case> cases = new ArrayList<>();
+      Scanner scanner = new Scanner(System.in);
+      int caseCount = scanner.nextInt();
+      for (int i = 0; i < caseCount; i++)
+         cases.add(new Case(scanner));
+      scanner.close();
+      return cases;
+   }
+
+   private static void process(List<Case> cases) {
+      for (Case cas : cases) {
+         System.out.println(cas.process());
+      }
+   }
+
+   // class is static so objects can be instantiated in read() a static method
+   static class Case {
+      int cycleCount;
+
+      public Case(Scanner scanner) {
+         cycleCount = scanner.nextInt();
+      }
+
+      public int process()
+      {
+         int growth = 1;
+         for (int i = 0; i < cycleCount; i++)
+         {
+            // spring growth: double in height
+            if (i % 2 == 0)
+               growth *= 2;
+            // summer growth: just add 1 meter
+            else
+               growth += 1;
+         }
+         return growth;
+      }
+   }
 }
